cba899bbe8bb2800e845c970d553174cfeac2ec1
dentl <boogdoesit@gmail.com>
2020-11-23 17:26:16 -0800

    attempt to add button functionality

--- etch.js
+++ etch.js
@@ -2,8 +2,13 @@ let container = document.getElementById("container");
 let modalBtn = document.getElementById("modalBtn");
 let modal0 = document.getElementById("myModal0");
 let div0 = document.querySelectorAll("div");
-let rows = document.getElementById("input0");
-/*let cols = document.getElementById("input0")*/
+let btn = document.getElementById("btn");
+let rows = 16;
+let cols = rows;
+
+
+
+
 
 
 function createGrid(rows, cols) {
@@ -14,14 +19,17 @@ function createGrid(rows, cols) {
     grid.addEventListener("mouseover", function colorGrid() {
       grid.style.backgroundColor = "green";
     });
-    grid.innerText = (i + 1);
+    /*grid.innerText = (i + 1);*/
     container.appendChild(grid).className = "grid-item";
   };
 };
 
-function getValue() {
-  let inputVal = document.getElementById("input0").value;
+btn.onclick = function(e) {
+  if (e.target == btn) {
+    let newGrid = prompt("What is the size of your new grid?");
+    let cols = newGrid;
+  }
 }
 
 
-createGrid( 5, 5);
+createGrid( rows, cols);
